60c01b2f395f4056ec469d9115952a65e28b94f5
Shiro Kawai <shiro@acm.org>
2019-02-12 19:36:16 -1000

    Make mexpr-env to generate EVAL* definition

--- tools/mexpr-env.scm
+++ tools/mexpr-env.scm
@@ -2,25 +2,39 @@
 ;; A helper tool to convert definitions M-expr source into an assoc list.
 ;;
 
-;; Usage: gosh tools/mexpr-env examples/eval.mx
-
+(use gauche.parseopt)
 (add-load-path ".." :relative)
-
-(define-syntax DEFINE
-  (syntax-rules ()
-    [(_ ((var expr) ...))
-     (begin (pprint '((var . expr) ...))
-            (undefined))]))
 
 (define (usage)
   (print "Usage: gosh tools/axiom-env [-e] MEXPR-SOURCE ...")
   (print "  Read MEXPR-SOURCE and writes out the definitions in an assoc list")
   (print "  that can be passed to EVAL as an environment.")
+  (print "  With -e option, generate a definition of EVAL*, which calls EVAL")
+  (print "  with the given environment.")
   (exit 1))
 
+(define *preamble* "")
+(define *postamble* "")
+
+(define (setup-evalfn)
+  (set! *preamble* "(DEFINE ((EVAL* (LAMBDA (X) (EVAL X '")
+  (set! *postamble* ")))))\n"))
+
+(define-syntax DEFINE
+  (syntax-rules ()
+    [(_ ((var expr) ...))
+     (begin 
+       (display *preamble*)
+       (pprint '((var . expr) ...))
+       (display *postamble*)
+       (undefined))]))
+
 (define (main args)
-  (if (null? (cdr args))
-    (usage)
-    (dolist [file (cdr args)]
-      (load file :paths '("."))))
-  0)
+  (let-args (cdr args) ([#f "e" => setup-evalfn]
+                        [else => (^ _ (usage))]
+                        . files)
+    (if (null? files)
+      (usage)
+      (dolist [file files]
+        (load file :paths '("."))))
+    0))
