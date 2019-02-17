ae3f4874fbfaa3fa6f26261a8be85f27070b0caf
Shiro Kawai <shiro@acm.org>
2019-02-17 00:43:35 -1000

    Core SUBRs

--- LISP1/5/runtime.scm
+++ LISP1/5/runtime.scm
@@ -21,8 +21,12 @@
 (define *NIL* (rlet1 nil (list 'ATOM *PNAME* "NIL" *APVAL*)
                 (set! (cddddr nil) (list nil))))
 
+(define *F* `(ATOM ,*PNAME* "F" ,*APVAL* *NIL*))
+(define *T* (rlet1 t (list 'ATOM *PNAME* "T" *APVAL*)
+              (set! (cddddr nil) (list t))))
+
 ;;
-;; Axioms
+;; Helper functions
 ;;
 
 (define ($atom? obj) (and (pair? obj) (eq? (car obj) 'ATOM)))
@@ -32,7 +36,8 @@
   (define (rec obj)
     (cond [(eq? obj *NIL*) '()]
           [($atom? obj) (string->symbol (cadr (member *PNAME* (cdr obj))))]
-          [else (cons (rec (car obj)) (rec (cdr obj)))]))
+          [(pair? obj) (cons (rec (car obj)) (rec (cdr obj)))]
+          [else (format "#[~s]" obj)]))
   (if (eq? obj *NIL*)
     'NIL
     (rec obj)))
@@ -50,3 +55,42 @@
         [(pair? obj) (cons ($scheme->lisp (car obj))
                            ($scheme->lisp (cdr obj)))]
         [else (errorf "Cannot convert ~s to LISP" obj)]))
+
+(define ($error msg . objs)
+  (apply error msg (map $lisp->scheme objs)))
+
+(define-syntax $define
+  (syntax-rules ()
+    [(_ symbol type value)
+     (let1 lsym ($scheme->lisp 'symbol)
+       (set! (cdr lsym)
+             (append! (cdr lsym) (list ($scheme->lisp 'type) value))))]))
+
+
+($define CAR SUBR
+         (^x (cond [(eq? x *NIL*) *NIL*]
+                   [($cons? x) (car x)]
+                   [else ($error "Cannot take CAR of an atom:" x)])))
+($define CDR SUBR
+         (^x (cond [(eq? x *NIL*) *NIL*]
+                   [($cons? x) (cdr x)]
+                   [else ($error "Cannot take CDR of an atom:" x)])))
+($define CONS SUBR (^[x y] (cons x y)))
+($define ATOM SUBR (^x (if ($atom? x) *T* *F*)))
+($define EQ SUBR (^[x y] (if (eq? x y) *T* *F*)))
+
+;; These two are not LISP1.5 public functions, but the minimal native
+;; functions, on top of which we can build other primitives in LISP.
+($define $GETPLIST SUBR
+         (^x (if ($atom? x)
+               (cdr x)
+               ($error "Atom required, but got:" x))))
+($define $PUTPLIST SUBR
+         (^[x p] (if ($atom? x)
+                   (begin (set-cdr! x p) *NIL*)
+                   ($error "Atom required, but got:" x))))
+
+($define ERROR SUBR
+         (^[msg obj]
+           (errorf "LISP Error: ~a:" ($lisp->scheme msg) ($lisp->scheme obj))))
+
