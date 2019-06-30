d82f6ebda4db3db355b5a9d758ede1a3f8a58723
Shiro Kawai <shiro@acm.org>
2019-06-30 02:55:02 -1000

    Add a simple tracing tool

---
+++ tools/trace.scm
@@ -0,0 +1,33 @@
+(use srfi-42)
+(use gauche.parameter)
+
+(define-class <traced> ()
+  ((orig-proc :init-keyword :orig-proc)
+   (name :init-keyword :name)))
+
+(define nesting (make-parameter 0))
+
+(define (indent) (make-string (* (nesting) 2)))
+
+(define-method object-apply ((t <traced>) . args)
+  (print #"~(indent)Calling ~(~ t'name) with args:")
+  (do-ec [: arg (index i) args]
+         (begin 
+           (format (current-output-port) "~a~2d: " (indent) i)
+           (pprint arg :length 6 :level 4)))
+  (rlet1 r (parameterize ((nesting (+ (nesting) 1)))
+             (apply (~ t'orig-proc) args))
+    (display #"~(indent)Result of ~(~ t'name): ")
+    (pprint r :length 6 :level 4)))
+
+(define-syntax trace
+  (syntax-rules ()
+    [(_ X) (set! X (%make-trace X 'X))]))
+
+(define (%make-trace proc name)
+  (when (is-a? proc <traced>)
+    (error "Already traced:" name))
+  (make <traced> :orig-proc proc :name name))
+
+
+  
